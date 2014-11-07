ca6305ee226c7c0e38ee46f9981496cacd588528
Dawid Królak <taavit@gmail.com>
2014-11-07 16:22:29 +0100

    Update TwigExtraExtension.php

--- DependencyInjection/TwigExtraExtension.php
+++ DependencyInjection/TwigExtraExtension.php
@@ -13,9 +13,6 @@ class TwigExtraExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
